14347d60e7038757ab2fa8e7c785289cb1875df5
Khasan Rashidov <khasanrashidov2002@gmail.com>
2024-11-28 13:23:07 +0500

    update API_BASE_URL: change to the new development API URL

--- src/variables.ts
+++ src/variables.ts
@@ -1,2 +1,2 @@
 // Change the value of API_BASE_URL to the URL of your API
-export const API_BASE_URL = 'https://56fkmb9c-7111.euw.devtunnels.ms';
+export const API_BASE_URL = 'https://8l4s2sg4-7111.euw.devtunnels.ms';
